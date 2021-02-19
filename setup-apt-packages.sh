98c5778fae233086d6d3ca4bf5b4948dbdde9637
ashn <me@ashn.dev>
2021-02-19 10:22:13 -0500

    Replace pylint with flake8
    Pylint would consume 300MB of ram and hand indefinitely for a Python
    file with less than 2000 lines. This kind of performance is completely
    unacceptable so I am switching to flake8 for linting instead.

--- setup-apt-packages.sh
+++ setup-apt-packages.sh
@@ -38,7 +38,7 @@ sudo apt-get -y install valgrind
 #sudo apt-get -y install qemu qemu-kvm # LATER: sudo adduser $USER kvm
 sudo apt-get -y install black   # Python3 code formatting
 sudo apt-get -y install mypy    # Python3 type checking
-sudo apt-get -y install pylint3 # Python3 linter
+sudo apt-get -y install flake8 # Python3 linter
 
 # Utilities
 sudo apt-get -y install ascii
