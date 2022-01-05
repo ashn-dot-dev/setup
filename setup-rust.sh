585b7c2022d11dd90ab13e4a160d5048f965ccec
ashn <me@ashn.dev>
2022-01-05 09:29:04 -0500

    setup-rust.sh will now update the Rust toolchain if rustup is already installed

--- setup-rust.sh
+++ setup-rust.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+if which rustup > /dev/null 1>&2; then
+    echo "Rust toolchain already installed. Updating..."
+    rustup update
+    exit
+fi
+
+echo "Downloading Rust toolchain..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
