f65b6dbd63662bef35ddf3eb963c1180df338754
ashn <me@ashn.dev>
2025-11-08 20:39:45 -0500

    Ad Go to brew packages

--- setup-brew-packages.sh
+++ setup-brew-packages.sh
@@ -8,6 +8,7 @@ brew -v
 brew install black
 brew install clang-format
 brew install cmake
+brew install go
 brew install kitty # After set font to 14.0 and then `kitten theme` to Solarized Dark
 brew install mypy
 brew install node # After run `npm install -g browserify` for Browserify
