4c82283df089fb21ea037fea42873cb6e0c27daf
ashn <me@ashn.dev>
2026-02-19 08:00:37 -0500

    Add better display to brew packages

--- setup-brew-packages.sh
+++ setup-brew-packages.sh
@@ -35,6 +35,25 @@ brew install vim
 brew install wget
 
 # Extra
+
 # Adobe Acrobat - required to read uni transcripts
 #brew install --cask adobe-acrobat-reader
+
 brew install --cask audacity
+
+# Better Display used for getting a 16:9 ratio for screenshots at work.
+# Normally, macOS uses a 16:10 ratio, but we want display captures to be
+# normalized to a 16:9 ratio.
+#
+# https://github.com/waydabber/BetterDisplay?tab=readme-ov-file#homebrew
+#
+# Under macOS system settings, the 1600x1000 resolution is the default that I
+# use as a daily driver. I use 1600x900 for recording / screenshots, which can
+# be set by going to the settings -> built-in display -> enable custom scaling
+# (checked) -> add custom 1600x900 display.
+#
+# I favorite 1600x1000 as #1 and 1600x900 as favorite #2.
+#
+# Instructions were adapted from this YouTube video:
+# https://www.youtube.com/watch?v=tPOwzTww_Xo
+brew install --cask betterdisplay
